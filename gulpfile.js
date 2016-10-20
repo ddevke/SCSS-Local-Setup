bc8ba3e4cc1c9f8392b302f51f42faafdd4e6353
Dhammadeep <ddevke@gmail.com>
2016-10-20 17:25:04 +0530

    changes in gulp files, added main.scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('sass/**/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
 });
